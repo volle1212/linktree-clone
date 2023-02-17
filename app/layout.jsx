5ccdac6f6c1dc4b1dfe775c70ca4ab9f01ec2b21
volle1212 <94704890+volle1212@users.noreply.github.com>
2023-02-17 08:56:59 +0100

    phone bg fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <head />
-      <body className={`h-screen bg-[url('../public/muster/1.jfif')] bg-no-repeat bg-cover bg-left`}>
+      <body className={`h-screen bg-[url('../public/muster/phone.jpg')] sm:bg-[url('../public/muster/1.jfif')] bg-no-repeat bg-cover`}>
         {children}
       </body>
     </html>
